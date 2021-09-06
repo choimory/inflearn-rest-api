4c659de97a82e7cb89bea1d0a5d208a401d4900a
choimory <43328589+choimory@users.noreply.github.com>
2021-09-06 21:50:46 +0900

    Initial commit

---
+++ src/test/java/com/choimory/inflearnrestapi/InflearnRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choimory.inflearnrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InflearnRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
