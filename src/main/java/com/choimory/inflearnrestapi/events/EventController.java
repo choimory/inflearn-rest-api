409efff86c97f137879b4497d9eaed41c742bd73
choimory <choimory@naver.com>
2021-10-04 20:19:22 +0900

    chap2.2. 201 응답받기

--- src/main/java/com/choimory/inflearnrestapi/events/EventController.java
+++ src/main/java/com/choimory/inflearnrestapi/events/EventController.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,14 +19,13 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 @RequiredArgsConstructor
 @RequestMapping(value = "/api/events", produces = MediaTypes.HAL_JSON_VALUE)
 public class EventController {
-
     @PostMapping
     public ResponseEntity<Event> createEvent(@RequestBody(required = false) Event param){
-        //HATEOAS
-        URI uri = linkTo(EventController.class).slash("${id}").toUri();
-
         //for test code
         param.setId(10L);
+
+        //HATEOAS
+        URI uri = linkTo(EventController.class).slash(param.getId()).toUri();
 
         return ResponseEntity.created(uri)
                 .body(param);
