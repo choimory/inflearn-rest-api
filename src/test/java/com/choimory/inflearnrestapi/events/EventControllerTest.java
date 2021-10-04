a909d980198caf94170f6eb8cb5c952ef23e5a7b
choimory <choimory@naver.com>
2021-10-04 18:51:05 +0900

    chap2.1

--- src/test/java/com/choimory/inflearnrestapi/events/EventControllerTest.java
+++ src/test/java/com/choimory/inflearnrestapi/events/EventControllerTest.java
@@ -1,5 +1,6 @@
 package com.choimory.inflearnrestapi.events;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -7,10 +8,11 @@ import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+//@RunWith(SpringRunner.class) JUnit4
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
 class EventControllerTest {
@@ -21,8 +23,9 @@ class EventControllerTest {
      * 이벤트 생성 테스트
      * @throws Exception - 컨트롤러에서 던지는 비즈니스 로직상의 예외
      */
-    void createEvent() throws Exception{
-        mockMvc.perform(post("")
+    @Test
+    void createEvent() throws Exception {
+        mockMvc.perform(post("/api/events")
                 .contentType(MediaType.APPLICATION_PROBLEM_JSON_UTF8)
                 .accept(MediaTypes.HAL_JSON))
                 .andExpect(status().isCreated());
